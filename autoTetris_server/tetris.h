38b934b2826d41598d768a68affe6cb167fe3ba2
GloomyMouse <gloomymouse@126.com>
2016-01-28 19:51:57 +0800

    autoTetris

---
+++ autoTetris_server/tetris.h
@@ -0,0 +1,34 @@
+#ifndef _TETRIS_H_
+#define _TETRIS_H_
+
+#include "vars.h"
+
+void initMap(block[][mapWidth]);
+
+int getHeapTop(block[][mapWidth]);
+
+Tetromino *initTetro(Tetromino*, int);
+
+void drawMap(block[][mapWidth], int);
+
+void drawNext(Tetromino*);
+
+bool determineCrash(block[][mapWidth], Tetromino*, int, int);
+
+bool determineBound(block[][mapWidth], Tetromino*, int);
+
+int clearLine(block[][mapWidth]);
+
+void reMap(block[][mapWidth], Tetromino*, int, int, int);
+
+void copyMap(block[][mapWidth], char*);
+
+bool gameOver(block[][mapWidth], Tetromino*, int);
+
+void sendMsg(int, char*);
+
+void recvMsg(int, char*);
+
+void autoTetrisServer(void*);
+
+#endif
