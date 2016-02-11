dfe2ffcd8958f57835fa7b6216faf64a974b43df
Frank Hu <humour114@gmail.com>
2016-02-11 18:14:52 +0800

    Nuwa V1.2.4: much faster action and testing script.
    AI: much faster action due to optimization of algorithm.
    Testing script: powerful testing interface write by argparse.
        - input from random sequence or file
        - output sequence to file for retest
        - verbose and pause mode for detailed analysis
        - debug mode to profile performance

--- AI/Nuwa/Nuwa.py
+++ AI/Nuwa/Nuwa.py
@@ -39,13 +39,15 @@ def ai_action(status, tetromino, next_mino, verbose=False, next=False):
     action_next_rot = 0
     for rotate in range(0, tetromino.max_rotate):
         for position in range(0, max_width_mino[rotate]):
-            new_status = generate(status, tetromino, position, rotate)
+            new_status = generate(status, tetromino,
+                                  position, rotate)
             for next_rot in range(0, next_mino.max_rotate):
-                for next_pos in range(0, max_width_next[next_rot]):
-                    next_status = generate(new_status, next_mino,
-                                           next_pos, next_rot)
-                    temp_score, temp_height, temp_under_blocks = \
-                            score(next_status, best_height, best_under_blocks)
+                for next_pos in range(max_width_next[next_rot] - 1, -1, -1):
+                # for next_pos in range(0, max_width_next[next_rot]):
+                    next_status = generate(
+                        new_status, next_mino, next_pos, next_rot)
+                    temp_score, temp_height, temp_under_blocks = score(
+                        next_status, best_height, best_under_blocks)
                     if temp_score < best_score:
                         # update temp variables
                         best_height = temp_height
@@ -59,7 +61,7 @@ def ai_action(status, tetromino, next_mino, verbose=False, next=False):
                             print(("mino is {}, best action is: {},{};"+\
                                    "next action is: {}, {}. score is {}.")\
                                 .format(tetromino.name, action_pos, action_rot,
-                                        action_next_pos, action_next_rot, 
+                                        action_next_pos, action_next_rot,
                                         best_score))
 
     if verbose:  # output action
@@ -90,27 +92,25 @@ def generate(status, tetromino, position, rotate):
         1. adding mino
         2. clear lines if needed
     """
+    status = copy_matrix(status)
     # prepare shape and profiles of status and mino
     status_profile = profile_status(status)
     # print('mino, pos, rotate:', tetromino.name, position, rotate) # debugging
     mino, mino_profile = tetromino.clean_up(rotate)
-    #calculate for columns
+    # calculate for columns: final mino height
     final_mino_height = 0
-    for i in range(0, len(mino)): #len(mino) is the width of mino
+    for i in range(0, len(mino)):  # len(mino) is the width of mino
         mino_height = status_profile[position + i] + mino_profile[i]
         if mino_height > final_mino_height:
             final_mino_height = mino_height
-    status_T = transpose(status)
     # adding mino
     for i in range(0, len(mino)):
         for j in range(0, len(mino[i])):
             if mino[i][j] is 1:
-                status_T[position + i]\
-                        [MAX_HEIGHT - final_mino_height - len(mino[i]) + j] = 1
+                status[MAX_HEIGHT - final_mino_height - len(mino[i]) + j]\
+                      [position + i] = 1
                 # line number is the same direction for mino and status
-    status = transpose(status_T)
     return status
-
 
 def clear(status):
     """Clear status, return new status and lines cleaned."""
@@ -122,6 +122,16 @@ def clear(status):
         status.insert(0, [0] * MAX_WIDTH)
 
     return status, num_cleared_rows
+
+
+def copy_matrix(matrix):
+    """Fast copy matrix (2d nested int list).
+
+    """
+    t = []
+    for i in matrix:
+        t.append(i[:])  # only apply for 2d-matrix
+    return t
 
 
 def transpose(matrix):
@@ -159,23 +169,18 @@ def score(status, best_height=MAX_HEIGHT, best_under_blocks=BIG_UNDER_BLOCKS):
         score_status += (height - 10) * 100
 
     # 2. under blocks and cut-off (for speed)
-    status_T = transpose(status)
-    s_underblocks, num_underblocks = score_underblocks(status, status_T)
+    # status_T = transpose(status)
+    s_underblocks, num_underblocks = score_underblocks(status)
     score_status += s_underblocks
     if num_underblocks - best_under_blocks > 2:
         # too many more under blocks, guess it's not a good position
         return BIG_SCORE, MAX_HEIGHT, BIG_UNDER_BLOCKS
 
     # 3. scan profile
-    profile = BLANK_LINE[:]
-    for column in range(0, MAX_WIDTH):
-        for row in range(0, MAX_HEIGHT):
-            if status_T[column][row] is not 0:
-                profile[column] = MAX_HEIGHT - row
-                break
+    profile = profile_status(status)
     # add minor height(profile height)
     for i in profile:
-        score_status += i  
+        score_status += i
     # add score of profile
     score_status += score_profile(profile)
 
@@ -188,8 +193,6 @@ def height_status(status):
     for i in range(0, MAX_HEIGHT):
         for j in range(0, MAX_WIDTH):
             if status[i][j]:
-            # if (j and j is not MAX_WIDTH - 1) and status[i][j]:
-                # ignore side height
                 height = MAX_HEIGHT - i
                 break
         if height is not 0: # find max height
@@ -199,17 +202,13 @@ def height_status(status):
 
 def profile_status(status):
     """Calculate profile of status."""
-    height = height_status(status)
-    status_T = transpose(status)
-    #find under blocks and profile (scan for each column)
-    profile = [0] * MAX_WIDTH # store profile
-    for column in range(0, len(status_T)):
-        flag = False
-        for block in status_T[column]:
-            if flag is False and block is not 0:
-                flag = True
+    # for tetris: the method of slice and change is better than append
+    profile = BLANK_LINE[:]
+    for column in range(0, MAX_WIDTH):
+        for row in range(0, MAX_HEIGHT):
+            if status[row][column] is not 0:
                 # calculate relative height, max height of blocks is 0
-                profile[column] = MAX_HEIGHT - status_T[column].index(block)
+                profile[column] = MAX_HEIGHT - row
                 break
     return profile
 
@@ -239,7 +238,7 @@ def score_profile(profile):
                 i += 2 # skip i+1 and i+2
         elif i is (MAX_WIDTH - 2): # right boundary: only 1 column gap
             if 0 - gap_right <= -2:
-                added_score += gap_right**2 * PUNISH_SINGLE_GAP 
+                added_score += gap_right**2 * PUNISH_SINGLE_GAP
             elif profile[i] - profile[i-1] <= -2:
                 added_score += (profile[i-1] - max(profile[i], profile[i+1]))\
                                 **2 * PUNISH_DOUBLE_GAP
@@ -259,12 +258,12 @@ def score_profile(profile):
 
     # punish max height - min height
     delta_h = max(profile) - min(profile)
-    if delta_h > 3:  
-        added_score += delta_h ** 2 
+    if delta_h > 3:
+        added_score += delta_h ** 2
     return added_score
 
 
-def score_underblocks(status, status_T):
+def score_underblocks(status):
     """Score under blocks.
 
     i: transposed status matrix, elements are columns.
@@ -276,14 +275,15 @@ def score_underblocks(status, status_T):
     SCORE_UNDERBLOCK = 30
     SCORE_STACK = 3
     SCORE_FIRST_AVAILABLE_ROW = 80
-    # fast init using numpy
-    under_block_matrix = BLANK_STATUS_ARRAY.tolist()  
+    # fast init using numpy, better than copy_matrix
+    under_block_matrix = BLANK_STATUS_ARRAY.tolist()
+    # under_block_matrix = copy_matrix(BLANK_STATUS)
 
     num_underblocks = 0
-    score = 0 
+    score = 0
     for column in range(0, MAX_WIDTH):
         num_underblocks, stacks = \
-            score_under_block_column(status_T[column], column, 
+            score_under_block_column(status, column,
                                      under_block_matrix)
         score += num_underblocks * SCORE_UNDERBLOCK
         for up, stack in stacks:
@@ -309,11 +309,11 @@ def score_underblocks(status, status_T):
     return score, num_underblocks
 
 
-def score_under_block_column(column, num_column, under_block_matrix):
+def score_under_block_column(status, num_column, under_block_matrix):
     """Score under blocks for a column.
-    
+
     i: list of column (len = MAX_HEIGHT)
-    o: 
+    o:
         1. num of under blocks
         2. list of (up_blocks, stacks)
     """
@@ -326,13 +326,13 @@ def score_under_block_column(column, num_column, under_block_matrix):
     for i in range(0, MAX_HEIGHT):
         if flag is False:
             # before any blocks appear
-            if column[i] is not 0:
+            if status[i][num_column] is not 0:
                 # first block in the column
                 flag = True
                 up_blocks += 1
                 stack += 1
         else:
-            if column[i] is 0:
+            if status[i][num_column] is 0:
                 num_underblocks += 1
                 if flag_stack is False:
                     # new under blocks
@@ -357,12 +357,6 @@ def score_under_block_column(column, num_column, under_block_matrix):
     return num_underblocks, up_stacks
 
 
-def build_test_case():
-    """Build test cases.
-    """
-    pass
-
-
 def print_status(status):
     """Print status matrix."""
     for row in status:
@@ -375,7 +369,7 @@ def print_status(status):
         print('|')
 
 
-def print_mino(tetromino, position, rotate):
+def print_mino(tetromino, position, rotate, add_blank=False):
     """print mino with position and rotate."""
 
     mino, mino_profile = Tetromino(tetromino).clean_up(rotate)
@@ -383,6 +377,9 @@ def print_mino(tetromino, position, rotate):
                   for j in range(0, position)] + mino
     print_mino = transpose(print_mino)
     # print row matrix for mino
+    if add_blank:
+        for i in range(4 - len(mino[0])):
+            print()
     for row in print_mino:
         print(' ', end='')
         for block in row:
@@ -393,56 +390,111 @@ def print_mino(tetromino, position, rotate):
         print()
 
 
-def main():
+def main(args):
     """Test AI using input list or random list of tetromino.
 
     Input: file input with single letter as a mino.
     Output: send these minos to AI script.
     """
-    test_line0 = [0 for i in range(0, MAX_WIDTH)]
-    test_status = [test_line0 for i in range(0, 20)]
-    MINO_LIST = 'sztjiol'
-    #score(test_status)
-    minolist = open('replay.txt').read()
+
+    # Handle arguments
+    minolist = False
+    if args.fromrandom and args.fromfile:
+        print('Conflict input of fromrandom and fromfile.',
+              'Override as input from random.')
+    elif args.fromrandom:
+        print('Input from random sequence.')
+    elif args.fromfile:
+        print('Input from file.')
+    else:
+        print('No input source. Override as input from random sequence.')
+        args.fromrandom = True
+    # Handle notes
+    if args.note:
+        if args.fromfile:
+            print('Conflict input and output to file. Override as no output.')
+            args.note = False
+        else:
+            replay = open('replay.txt', 'w')
+    if args.short:
+        import os
+        os.system('clear')
+
+    # init blank
+    test_status = BLANK_STATUS_ARRAY.tolist()
+
     i = 0
     score = 0
-    import time
-    import random
-    mino = random.choice(MINO_LIST)
-    next_mino = random.choice(MINO_LIST)
+    MINO_LIST = 'sztjiol'
 
+    if args.fromrandom:
+        import random
+        mino = random.choice(MINO_LIST)
+    else:
+        minolist = open('replay.txt').read()
+        mino = minolist[i]
+    if args.note:
+        replay.write(mino)
+
+    import time
     while True:
-        # input from random choice
-        next_mino = random.choice(MINO_LIST)
-        # input from minolist
-        # mino = minolist[i]
-        # next_mino = minolist[i+1]
-        # if input('press enter to get next tetromino.') is not '':
-        #     break
+        # input of next mino
+        if args.fromrandom:
+            next_mino = random.choice(MINO_LIST)
+        else:
+            try :
+                next_mino = minolist[i+1]
+            except IndexError:
+                print('Input file finished.')
+                break
+            else:
+                if next_mino not in MINO_LIST:
+                    print('Input tetromino is wrong.')
+                    break
+
+        # Handle pause, note
+        if args.pause:
+            if input('press enter to get next tetromino.') is not '':
+                break
             # press enter for next mino
-        print('mino is {}, next mino is {}'.format(mino, next_mino))
-        print_mino(mino, 0, 0)
-        print_mino(next_mino, 15, 0)
+        if args.note:
+            replay.write(next_mino)
 
         t0 = time.time()
-        action_pos, action_rot = \
-            ai_action(test_status, mino, next_mino, verbose=False, next=False)
 
-        # # verbose next output
-        # action_pos, action_rot, action_next_pos, action_next_rot = \
-        #     ai_action(test_status, mino, next_mino, verbose=False, next=True)
-        # print_mino(next_mino, action_next_pos, action_next_rot)
+        # verbose output
+        if args.verbose or args.vv:
+            print('mino is {}, next mino is {}'.format(mino, next_mino))
+            print_mino(mino, 0, 0)
+            print_mino(next_mino, 15, 0)
+            if args.vv:
+                action_pos, action_rot, action_next_pos, action_next_rot = \
+                    ai_action(test_status, mino, next_mino,
+                              verbose=True, next=True)
+            else:
+                action_pos, action_rot, action_next_pos, action_next_rot = \
+                    ai_action(test_status, mino, next_mino,
+                              verbose=False, next=True)
+        else:
+            action_pos, action_rot = \
+                ai_action(test_status, mino, next_mino,
+                          verbose=False, next=False)
 
-        print_mino(mino, action_pos, action_rot)
-        print_status(test_status)            
         test_status = generate(test_status, Tetromino(mino),
                                action_pos, action_rot)
-
         test_status, cleaned_rows = clear(test_status)
+
+        if args.verbose or args.vv:
+            print_status(test_status)
+            print_mino(mino, action_pos, action_rot)
+            print_mino(next_mino, action_next_pos, action_next_rot)
+
+        # calculate height and judge game over
         height = height_status(test_status)
         if height >= MAX_HEIGHT:
             print("Game over! score: {}".format(score))
             break
+
         # add score display
         if cleaned_rows > 0:
             if cleaned_rows is 1:
@@ -450,26 +502,61 @@ def main():
             elif cleaned_rows is 2:
                 score += 40
             elif cleaned_rows is 3:
-                score += 90
+                score += 120
             else :
-                score += 200
+                score += 300
         t1 = time.time()
-        print("AI reaction time {:.3f}s".format(t1-t0))
+        if args.short:
+            os.system('clear')
+            print_mino(mino, action_pos, action_rot, add_blank=True)
+            print_status(test_status)
+        print("Nuwa: reaction time {:.3f}s".format(t1-t0))
+        print("Number of blocks: {}, score: {}".format(i, score))
 
         i += 1
-        print("number of blocks: {}, score: {}".format(i, score)) 
         mino = next_mino
-
-        #if i > 50: 
-        #    break
+        # code for short test
+        # if i >= 100:
+        #     print_status(test_status)
+        #     break
 
 
 if __name__ == '__main__':
-    # profile and state script
-    # import cProfile
-    # cProfile.run('main()', 'restats')
-    # import pstats
-    # p = pstats.Stats('restats')
-    # p.sort_stats('cumulative').print_stats()
-
-    main()
+    import argparse
+    # parse arugments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--fromrandom',
+                        help='Random input sequence for Nuwa.',
+                        action='store_true')
+    parser.add_argument('-f', '--fromfile',
+                        help='Input sequence from file for Nuwa. Input should be single letter sequence.',
+                        action='store_true')
+    parser.add_argument('-n', '--note',
+                        help='Note sequence of minos to "replay.txt".',
+                        action='store_true')
+    parser.add_argument('-p', '--pause',
+                        help='Pause sequence of minos for every tetromino. \
+                             \Press enter to go on, other key to quit.',
+                        action='store_true')
+    parser.add_argument('-v', '--verbose',
+                        help='Show more AI action details.',
+                        action='store_true')
+    parser.add_argument('--vv',
+                        help='Show very verbose AI action details.',
+                        action='store_true')
+    parser.add_argument('-s', '--short',
+                        help='Show less AI action details. Only show status',
+                        action='store_true')
+    parser.add_argument('-d', '--debug',
+                        help="Profile Nuwa's actions.",
+                        action='store_true')
+    args = parser.parse_args()
+    if args.debug:
+        # profile and state script
+        import cProfile
+        cProfile.run('main(args)', 'restats')
+        import pstats
+        p = pstats.Stats('restats')
+        p.sort_stats('cumulative').print_stats(20)
+    else:
+        main(args)
